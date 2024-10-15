125291e0710d2a26c276dcc1cb13d61dbf379cba
lennikorhonen <lenni.korhonen2@gmail.com>
2024-10-15 19:45:22 +0300

    Seperate wifi_init code from main function and add main loop

--- main/wifi_scanner.c
+++ main/wifi_scanner.c
@@ -9,10 +9,7 @@
 #include "esp_wifi.h"
 #include "esp_wifi_types.h"
 #include "nvs_flash.h"
-
-// TODO: 
-// - [ ] Clean up the code to functions, like intializing the wifi
-// - [ ] Maybe add some type of main loop for. ex. every 5 minutes loop for APs
+#include "portmacro.h"
 
 char* auth_mode_to_char(wifi_auth_mode_t mode) {
     char *modes[] = {
@@ -34,11 +31,8 @@ char* auth_mode_to_char(wifi_auth_mode_t mode) {
     return modes[mode];
 }
 
-void app_main(void)
-{
+void wifi_init() {
     char* taskName = pcTaskGetName(NULL);
-    ESP_LOGI(taskName, "Setting up"); // Log that we are on setup
-
     ESP_ERROR_CHECK(nvs_flash_init());
 
     wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
@@ -48,6 +42,14 @@ void app_main(void)
     ESP_LOGI(taskName, "Wifi initialized!");
     ESP_ERROR_CHECK(esp_wifi_start());
     ESP_LOGI(taskName, "Wifi started!");
+}
+
+void app_main(void)
+{
+    char* taskName = pcTaskGetName(NULL);
+    ESP_LOGI(taskName, "Setting up"); // Log that we are on setup
+
+    wifi_init();
 
     const wifi_scan_config_t scan_config = {
         .ssid = 0,
@@ -57,28 +59,33 @@ void app_main(void)
     };
 
     ESP_LOGI(taskName, "Set up done. Starting scan now!"); // Log that we have done our setup
-    // Scan wifi APs
-    ESP_ERROR_CHECK(esp_wifi_scan_start(&scan_config, true));
 
-    ESP_LOGI(taskName, "Scan done. Getting scan info now!");
+    // main loop
+    for (;;) {
+        // Scan wifi APs
+        ESP_ERROR_CHECK(esp_wifi_scan_start(&scan_config, true));
+        ESP_LOGI(taskName, "Scan done. Getting scan info now!");
 
-    uint16_t num_of_aps;
-    esp_wifi_scan_get_ap_num(&num_of_aps); // Store the number of found APs to num_of_aps
+        uint16_t num_of_aps; // variable to save the number of found APs
+        esp_wifi_scan_get_ap_num(&num_of_aps); // Store the number of found APs to num_of_aps
 
-    wifi_ap_record_t ap_recs[num_of_aps];
-    esp_wifi_scan_get_ap_records(&num_of_aps, ap_recs); // Store the found AP records to ap_recs
+        wifi_ap_record_t ap_recs[num_of_aps]; // Variable to save the found AP records with length of found APs
+        esp_wifi_scan_get_ap_records(&num_of_aps, ap_recs); // Store the found AP records to ap_recs
 
-    if (num_of_aps == 0) {
-        printf("Didn't find any APs\n");
-    } else {
-        printf("Found %d access points\n", num_of_aps);
-        printf("\n");
-        printf(" SSID                 | Channel | Signal strength | Auth mode \n");
-        printf("==============================================================\n");
-        for (int i = 0; i < num_of_aps; i++) { // Main loop
-            // Log wifi APs
-            printf(" %20s | %7d | %15d | %9s\n", (char *)ap_recs[i].ssid, ap_recs[i].primary, ap_recs[i].rssi, auth_mode_to_char(ap_recs[i].authmode));
+        if (num_of_aps == 0) {
+            printf("Didn't find any APs\n");
+        } else {
+            printf("Found %d access points\n", num_of_aps);
+            printf("\n");
+            printf(" SSID                 | Channel | Signal strength | Auth mode \n");
+            printf("==============================================================\n");
+            for (int i = 0; i < num_of_aps; i++) { // Main loop
+                // Log wifi APs
+                printf(" %20s | %7d | %15d | %9s\n", (char *)ap_recs[i].ssid, ap_recs[i].primary, ap_recs[i].rssi, auth_mode_to_char(ap_recs[i].authmode));
+            }
+            printf("==============================================================\n");
         }
-        printf("==============================================================\n");
+
+        vTaskDelay(300000 / portTICK_PERIOD_MS); // Delay for 5 minute between each scan
     }
 }
